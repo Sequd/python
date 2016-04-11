a30bdf77fba7c92ff07e88ef682dde6c3d09a817
evgeniy.korunov <e.korunov@gmail.com>
2016-04-11 16:36:53 +0400

    Примеры по матрицам и линейной алгебре

--- Steps/ExampleTest2.py
+++ Steps/ExampleTest2.py
@@ -9,3 +9,12 @@ data = np.loadtxt(f, usecols=(0, 1, 2, 3), skiprows=1, delimiter=",")
 # print(sbux[0:4])
 print(data.mean(axis=0))
 print(data.mean())
+
+x = np.array([[1, 60], [1, 50], [1, 75]])
+y = np.array([[10], [7], [12]])
+step1 = x.T.dot(x)
+step2 = np.linalg.inv(step1)
+step3 = step2.dot(x.T)
+H = step3.dot(y)
+
+print(H)
