620ac4b46ed9dfe0379ae1eb257451a52280be7e
evgeniy.korunov <e.korunov@gmail.com>
2016-04-14 16:40:37 +0400

    Обучение парцептрона

--- Steps/Parceptron2.py
+++ Steps/Parceptron2.py
@@ -1,9 +1,14 @@
 import numpy as np
+import random
 
 wv = np.array([0, 0, 0], dtype=float)
 full = np.array([[1, 1, 0.3], [1, 0.4, 0.5], [1, 0.7, 0.8]], dtype=float)
 res = np.array([1, 1, 0])
 index = 0
+
+a = np.array((1, 2), dtype=int)
+print(a)
+print(a.shape)
 
 
 def process(step):
