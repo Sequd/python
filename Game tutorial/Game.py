389a8fa4b99f8838e40ce242b50f52833dff2a95
Evgeniy <e.korunov@gmail.com>
2019-01-06 20:27:58 +0300

    [+] game tutorial

--- Game tutorial/Game.py
+++ Game tutorial/Game.py
@@ -1,28 +1,74 @@
+import math
 import turtle
-import random
 
 windows = turtle.Screen()
 windows.setup(1200 + 3, 800 + 3)
-windows.bgcolor('blue')
+windows.bgcolor('#3d3b47')
 windows.screensize(1200, 800)
 
-pen = turtle.Turtle()
-pen.color('red')
+# Начальные координаты базы
+BASE_X, BASE_Y = 0, -300
+missiles = []
 
 
-# pen.shape('turtle')
+def fire_missale(x, y):
+    """Конструируем и стреляем ракету"""
+    global missile
+    missile = turtle.Turtle()
+    missile.hideturtle()
+    missile.speed(0)
+    missile.color('white')
+    missile.penup()
+    missile.setpos(x=BASE_X, y=BASE_Y)
+    missile.pendown()
+    alpha = calc_alpha(x1=BASE_X, y1=BASE_Y, x2=x, y2=y)
+    missile.setheading(alpha)
+    missile.showturtle()
+    # missile.forward(500)
+    # missile.shape('circle')
+    # missile.shapesize(2)
+    # missile.shapesize(3)
+    # missile.shapesize(4)
+    # missile.shapesize(5)
+    # missile.clear()
+    # missile.hideturtle()
 
-def airplane(y):
-    if y < 0:
-        pen.color('yellow')
-    for x in [-200, 0, 200]:
-        pen.penup()
-        pen.setpos(x, y)
-        pen.pendown()
-        pen.circle(random.randint(50, 100))
+    info = {'m': missile, 't': [x, y], 's': 'launch', 'r': 0}
+    missiles.append(info)
 
 
-airplane(100)
-airplane(-100)
+def calc_alpha(x1, y1, x2, y2):
+    dx = (x2 - x1)
+    dy = (y2 - y1)
+    length = (dx ** 2 + dy ** 2) ** .5
+    cos_alpha = dx / length
+    alpha = math.acos(cos_alpha)
+    alpha = math.degrees(alpha)
+    return alpha
 
-windows.mainloop()
+
+windows.onclick(fire_missale)
+
+# windows.mainloop()
+
+while True:
+    windows.update()
+
+    for info in missiles:
+        missile = info['m']
+        state = info['s']
+
+        if state == 'launch':
+            missile.forward(4)
+            target = info['t']
+            if missile.distance(x=target[0], y=target[1]) <= 20:
+                info['s'] = 'explode'
+                missile.shape('circle')
+        elif state == 'explode':
+            radius = info['r'] + 1
+            missile.shapesize(radius)
+            info['r'] = radius
+            if radius > 5:
+                missiles.remove(info)
+                missile.clear()
+                missile.hideturtle()
