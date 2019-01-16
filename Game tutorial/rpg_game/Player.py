0ee681c6107afb4e0cce388302ef0f585375c2c8
Evgeniy <e.korunov@gmail.com>
2019-01-17 00:05:18 +0300

    [+] rpg game tutorial

--- Game tutorial/rpg_game/Player.py
+++ Game tutorial/rpg_game/Player.py
@@ -7,26 +7,58 @@ class Player():
     def __init__(self, screen):
         self.screen = screen
         self.direction = RIGHT
+        self.hp = MAX_HP
+        self.mp = MAX_MP
         self.state = ALIVE
         self.position = [START_X, START_Y, RIGHT]
         self.name = 'Player 1'
-        self.image_pack = ['./man_right.jpg', './man_down.jpg', './man_left.jpg', './man_up.jpg']
+        self.image_pack = ['./man_right.jpg', './man_down.jpg', './man_left.jpg', './man_up.jpg', './health_bar.jpg',
+                           './health_point.jpg', './mana_bar.jpg', './mana_point.jpg']
         self.images = []
         for image in self.image_pack:
             i = pygame.image.load(image)
             self.images.append(i)
+
         # load from sets
         # for image in range(0, 4):
         #     temp = pygame.image.load(image).convert_alpha()
         #     i = []
-        #     i.append(temp.subsurface(0, 0, 55, 55))
-        self.mooving = [0, 0, 0, 0]
+        #     i.append(temp.subsurface(0, 0, 55, 55)) # загрузка куска изображения
+        self.moving = [0, 0, 0, 0]
 
-    def moov(self):
-        pass
+    def move(self):
+        if self.moving[UP] == 1:
+            self.position[Y] -= PLAYER_SPEED
+        if self.moving[DOWN] == 1:
+            self.position[Y] += PLAYER_SPEED
+        if self.moving[RIGHT] == 1:
+            self.position[X] += PLAYER_SPEED
+        if self.moving[LEFT] == 1:
+            self.position[X] -= PLAYER_SPEED
+
+        if self.position[Y] < 0:
+            self.position[Y] = 0
+        if self.position[Y] > SCREEN_HEIGHT - 55:
+            self.position[Y] = SCREEN_HEIGHT - 55
+        if self.position[X] < 0:
+            self.position[X] = 0
+        if self.position[X] > SCREEN_WIDTH - 50:
+            self.position[X] = SCREEN_WIDTH - 50
+
+    def lose_hp(self, damage):
+        self.hp -= damage
 
     def render(self):
         self.screen.blit(self.images[self.direction], (self.position[X], self.position[Y]))
+        self.render_ui()
 
     def render_ui(self):
-        pass
+        self.screen.blit(self.images[HEALTH_BAR], (self.position[X], self.position[Y] + 60))
+        d_hp = int(self.hp / HEALTH_BAR_SIZE / HEALTH_POINT_SIZE)
+        for i, p in enumerate(range(0, d_hp)):
+            self.screen.blit(self.images[HEALTH_POINT], (self.position[X] + (i * HEALTH_POINT_SIZE), self.position[Y] + 60))
+
+        self.screen.blit(self.images[MP_BAR], (self.position[X], self.position[Y] + 67))
+        d_mp = int(self.mp / MP_BAR_SIZE / MP_POINT_SIZE)
+        for i, p in enumerate(range(0, d_mp)):
+            self.screen.blit(self.images[MP_POINT], (self.position[X] + (i * MP_POINT_SIZE), self.position[Y] + 67))
--- Game tutorial/rpg_game/main.py
+++ Game tutorial/rpg_game/main.py
@@ -13,7 +13,38 @@ class Main():
         self.main_loop()
 
     def handle_event(self):
-        pass
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.running = False
+
+            # Действие при нажатии кнопки
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    self.player.position[D] = RIGHT
+                    self.player.moving = [1, 0, 0, 0]
+                if event.key == pygame.K_DOWN:
+                    self.player.position[D] = DOWN
+                    self.player.moving = [0, 1, 0, 0]
+                if event.key == pygame.K_LEFT:
+                    self.player.position[D] = LEFT
+                    self.player.moving = [0, 0, 1, 0]
+                if event.key == pygame.K_UP:
+                    self.player.position[D] = UP
+                    self.player.moving = [0, 0, 0, 1]
+
+            # Действие при отжатии
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    self.player.moving[RIGHT] = 0
+                if event.key == pygame.K_DOWN:
+                    self.player.moving[DOWN] = 0
+                if event.key == pygame.K_LEFT:
+                    self.player.moving[LEFT] = 0
+                if event.key == pygame.K_UP:
+                    self.player.moving[UP] = 0
+
+                if event.key == pygame.K_SPACE:
+                    self.player.lose_hp(5)
 
     def render(self):
         self.screen.blit(self.background, (0, 0))
@@ -23,15 +54,13 @@ class Main():
 
     def main_loop(self):
         while self.running:
-            pygame.time.delay(40)
+            # pygame.time.delay(40)
+            self.player.move()
             self.render()
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    self.running = False
+            self.handle_event()
 
 
 pygame.init()
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-# screen.fill((30, 40, 50))
 pygame.display.set_caption('rpg game')
 game = Main(screen)
