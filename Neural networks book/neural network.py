6efe5ed920e943785117424122f25054bfd3a5ac
sequd <e.korunov@gmail.com>
2020-03-28 18:17:05 +0300

    [+] dataset files, example NeuralNetwork

--- Neural networks book/neural network.py
+++ Neural networks book/neural network.py
@@ -15,20 +15,58 @@ class NeuralNetwork:
         # коэффициент обучения
         self.lr = lerningRate
 
+        # w_i_j
+        # w11 w21
+        # w12 w22 etc
         # матрици весов связей wih (входной-скрытый)
         self.wih = (numpy.random.rand(self.hidden, self.input) - 0.5)
 
         # матрици весов связей who (скрытый-выходной)
         self.who = (numpy.random.rand(self.output, self.hidden) - 0.5)
 
+        # self.wih = numpy.random.normal(0.0, pow(self.inodes, -0.5), (self.hnodes, self.inodes))
+        # self.who = numpy.random.normal(0.0, pow(self.hnodes, -0.5), (self.onodes, self.hnodes))
+
         # сигмоида в качестве функции активации
         self.activation_function = lambda x: scipy.special.expit(x)
 
         pass
 
-    def train(self):
+    def train(self, input_list, target_list):
         """тренировка сети"""
-        pass
+
+        # преобразовать список входных значений в думерный массив
+        inputs = numpy.array(input_list, ndmin=2).T
+        target = numpy.array(target_list, ndmin=2).T
+
+        # рассчитать входящие сигналы для скрытого слоя
+        hidden_inputs = numpy.dot(self.wih, inputs)
+
+        # рассчитать исходящие сигналы для скрытого слоя
+        hidden_outputs = self.activation_function(hidden_inputs)
+
+        # рассчитать входящие сигналы для выходного слоя
+        final_inputs = numpy.dot(self.who, hidden_outputs)
+        # рассчитать исходящие сигналы для выходного слоя
+        final_outputs = self.activation_function(final_inputs)
+
+        # ошибка = целевое значение - фактическое значение
+        output_errors = target - final_outputs
+
+        # ошибка скрытого слоя - это ошибки output_errors,
+        # распределенные пропорцианально весовым коэффицентам связей
+        # и рекомбинированные на скрытых узлах
+        hidden_errors = numpy.dot(self.who.T, output_errors)
+
+        # обновить весовые коэффициенты связей между скрытым и выходным слоями
+        self.who += self.lr * numpy.dot(
+            (output_errors * final_outputs * (1.0 - final_outputs)),
+            numpy.transpose(hidden_outputs))
+
+        # обновить весовые коэффициенты связей между входным и скрытым слоями
+        self.wih += self.lr * numpy.dot(
+            (hidden_outputs * hidden_outputs * (1.0 - hidden_outputs)),
+            numpy.transpose(inputs))
 
     def query(self, inputs):
         """опрос сети"""
@@ -46,11 +84,11 @@ class NeuralNetwork:
         return final_outputs
 
 
-input = 3
-hidden = 3
-output = 3
+input_number = 3
+hidden_number = 3
+output_number = 3
 rate = 0.3
 
-n = NeuralNetwork(input, hidden, output, rate)
+n = NeuralNetwork(input_number, hidden_number, output_number, rate)
 final = n.query([1.0, 0.5, -1.5])
 print(final)
